01525a50f22527c4c128408e212f0f85c2afc396
putsenkoinna-byte <putsenkoinna@gmail.com>
2026-07-27 16:41:56 +0500

    Create winners.js

---
+++ winners.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Победители конкурса</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+  <h1>🏆 Победители конкурса</h1>
+</header>
+
+
+<div id="winners" class="works-grid"></div>
+
+
+<script src="winners.js"></script>
+
+</body>
+</html>
