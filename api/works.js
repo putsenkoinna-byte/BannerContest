68d16b30d43d9e9ef062cae1aa5eae0b081586b3
putsenkoinna-byte <putsenkoinna@gmail.com>
2026-07-27 07:53:41 +0500

    Update works.js

--- api/works.js
+++ api/works.js
@@ -1,23 +1,23 @@
 export default async function handler(req, res) {
-  try {
-    const token = process.env.AIRTABLE_TOKEN;
 
-    const response = await fetch(
-      "https://api.airtable.com/v0/appSxQXkQdS7Z9V6b/tblw7e0Mp2HGsym5n",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
-    );
+  const token = process.env.AIRTABLE_TOKEN;
 
-    const data = await response.json();
-
-    res.status(200).json(data);
-
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
+  if (!token) {
+    return res.status(500).json({
+      error: "TOKEN NOT FOUND"
     });
   }
+
+  const response = await fetch(
+    "https://api.airtable.com/v0/appSxQXkQdS7Z9V6b/tblw7e0Mp2HGsym5n",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
