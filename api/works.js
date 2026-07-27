83a4b38bf2103e01902811fe9b73201d7a6215f5
putsenkoinna-byte <putsenkoinna@gmail.com>
2026-07-27 07:28:19 +0500

    Update works.js

--- api/works.js
+++ api/works.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
 
+  const token = process.env.AIRTABLE_TOKEN;
+
+  const baseId = "appSxQXkQdS7Z9V6b";
+  const tableId = "tblw7e0Mp2HGsym5n";
+
+  const response = await fetch(
+    `https://api.airtable.com/v0/${baseId}/${tableId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
